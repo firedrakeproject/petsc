adb95c82266dc17747c2cf76fb7e4466ebaf7eb5
Jack Betteridge <J.D.Betteridge@bath.ac.uk>
2021-10-14 19:03:45 +0100

    Added hash table PetscInt --> PetscObject *

---
+++ include/petsc/private/hashmapobj.h
@@ -0,0 +1,11 @@
+#if !defined(PETSC_HASHMAPP_H)
+#define PETSC_HASHMAPP_H
+
+#include <petsc/private/hashmap.h>
+
+/*
+ * Hash map from PetscInt --> PetscObject *
+ * */
+PETSC_HASH_MAP(HMapObj, PetscInt, PetscObject *, PetscHashInt, PetscHashEqual, NULL)
+
+#endif /* PETSC_HASHMAPP_H */
